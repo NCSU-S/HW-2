da01a85690a45324fa546ab999ffed9d2bb81638
Weiran Fu <fuweiran816@gmail.com>
2020-08-24 01:27:10 -0400

    add homeScreen

---
+++ code/swift/README.md
@@ -0,0 +1,9 @@
+## Download Swift
+Swift doesn't support Windows, so if you are not in macOS, please download Swift in Linux.
+
+[Swift Download](https://swift.org/download/)
+
+## How to run
+`$ swift build`
+
+`$ swift run`
--- code/swift/Source/GOL/main.swift
+++ code/swift/Source/GOL/main.swift
@@ -1,11 +1,30 @@
+/*
+usage: gawk -f life.awk
+Then hit and hold the return key.
+For each key press, you get one new generation.
+
+Each generation, a cell C is alive 1 or dead 0.
+In the next generation each cell C is alive or dead
+depending on a count of its neighbours N
+
+  Now Neighbors           Next
+  --- ---------           --------------
+  1   0,1             ->  0  # Lonely
+  1   4,5,6,7,8       ->  0  # Overcrowded
+  1   2,3             ->  1  # Lives
+  0   3               ->  1  # It takes three to give birth!
+  0   0,1,2,4,5,6,7,8 ->  0  # Barren
+
+Code citation : https://gist.github.com/timm/1f4e45d46e4788ee43f12ebe54409b2f#file-life-awk
+*/
+
 import Foundation
 
 func main() {
   usleep(1000000)
+  clearScreen()
   life(rows: 50, cols: 20, some: 0.619, gens: 200)
 }
-
-
 
 func life(rows: Int, cols: Int, some: Double, gens: Int) {  
   var array = [Int]()
@@ -18,7 +37,8 @@ func life(rows: Int, cols: Int, some: Double, gens: Int) {
 
 func live(a: [Int], rows: Int, gen: Int) {
   if gen < 1 {return}
-  usleep(100000)
+  sleep()
+  homeScreen()
   print(NSString(format:"Generation %3d", gen))
   for c in 1...a.count {
     print(a[c-1] == 1 ? "o" : " ", terminator:"")
@@ -47,5 +67,9 @@ func getValue(array: [Int], index: Int) -> Int {
     return array[index]
   }
 }
+
+func sleep() {usleep(100000)}
+func homeScreen() {print("\033[1;1H")}
+func clearScreen() {print("\033[2J")}
 
 main()
